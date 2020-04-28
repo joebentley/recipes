acfd6545cf7200b75aea046343c639d8740db1ac
Joe Bentley <jbentley@star.sr.bham.ac.uk>
2020-04-28 13:23:30 +0100

    Set up basics for adding recipes and ingredients

---
+++ db/migrate/20200427212013_create_ingredients.rb
@@ -0,0 +1,11 @@
+class CreateIngredients < ActiveRecord::Migration[6.0]
+  def change
+    create_table :ingredients do |t|
+      t.string :name
+      t.string :quantity
+      t.references :recipe, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
